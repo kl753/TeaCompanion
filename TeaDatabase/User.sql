05bafd1517940d80f1ddd2be41fe6ed7907e7dcb
kl753 <cyberjade@gmail.com>
2025-06-12 00:56:36 -0500

    Database creation.

---
+++ TeaDatabase/User.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserId] INT NOT NULL PRIMARY KEY, 
+    [UserName] NVARCHAR(50) UNIQUE NOT NULL, 
+    [Email] NVARCHAR(50) NOT NULL, 
+    [PasswordHash] NVARCHAR(50) NOT NULL, 
+    [FirstName] NCHAR(10) NULL, 
+    [LastName] NCHAR(10) NULL, 
+    [JoinDate] TIMESTAMP NOT NULL, 
+    [LastLogin] TIMESTAMP NULL, 
+    [IsAdmin] BIT NULL DEFAULT 0,
+)
