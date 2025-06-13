ec99a7364ab5c4ae6348c861ab355f9da172ef29
kl753 <cyberjade@gmail.com>
2025-06-13 14:01:55 -0500

    Add table

---
+++ TeaDatabase/BrewSession.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[BrewSession]
+(
+	[BrewID] INT NOT NULL PRIMARY KEY, 
+    [StashItemID] INT NOT NULL,
+	[UserID] int NOT NULL, 
+    [Timestamp] TIMESTAMP NOT NULL, 
+    [Temp] INT NOT NULL, --Celsius
+    [SteepTime] INT NOT NULL, --Seconds 
+    [WaterAmount] INT NULL, --Mls
+    [UserRating] INT NULL, 
+    [BrewNotes] TEXT NULL, 
+    [BrewMethod] TEXT NULL, 
+    FOREIGN KEY ([StashItemID]) REFERENCES [dbo].[UserStash] ([StashItemID]),
+	FOREIGN KEY ([UserID]) REFERENCES [dbo].[User] ([UserID]),
+)
---
+++ TeaDatabase/UserStash.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[UserStash]
+(
+	[StashItemID] INT NOT NULL PRIMARY KEY, 
+    [UserID] INT NOT NULL,
+	[TeaID] INT NOT NULL, 
+    [Quanity] FLOAT NULL, --In grams 
+    [PurchaseDate] DATE NULL, 
+    [Source] NVARCHAR(50) NULL, 
+    [StorageNotes] TEXT NULL, 
+    [PersonalNotes] TEXT NULL, 
+    [IsCustomTea] BIT NULL DEFAULT 0, 
+    [CustomDescription] TEXT NULL, 
+    FOREIGN KEY ([UserID]) REFERENCES [dbo].[User] ([UserID]),
+	FOREIGN KEY ([TeaID]) REFERENCES [dbo].[Tea] ([TeaID]),
+
+)
