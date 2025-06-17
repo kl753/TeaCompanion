252de1e8a36c65c34a71b5c31a545d1f92d8e11d
kl753 <cyberjade@gmail.com>
2025-06-17 15:04:21 -0500

    Add controller

---
+++ TeaAPI/Mappers/MappingProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TeaAPI.Infrastructure;
+using TeaAPI.Models;
+
+namespace TeaAPI.Mappers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            // Create mappings between DTOs and Models
+            //Source -> Destination
+            CreateMap<Tea, TeaDTO>().ReverseMap(); // For TeaDTO <-> Tea
+            CreateMap<CreateTeaDTO, Tea>(); // For CreateTeaDTO -> Tea
+            CreateMap<UpdateTeaDTO, Tea>(); // For UpdateTeaDTO -> Tea
+        }
+    }
+}
