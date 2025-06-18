1e976ada041fc9e39e4ae576bf937c54c77f4a3b
kl753 <cyberjade@gmail.com>
2025-06-18 13:43:46 -0500

    Add repository pattern for UserTeaStash and BrewLog.

---
+++ TeaAPI/Interfaces/IBrewLogRepository.cs
@@ -0,0 +1,15 @@
+﻿using TeaAPI.Models;
+
+namespace TeaAPI.Interfaces
+{
+    public interface IBrewLogRepository
+    {
+        Task<IEnumerable<BrewLog>> GetAllBrewLogsForUserAsync(int userId); // Get all brew logs
+        Task<IEnumerable<BrewLog>> GetBrewLogsForUserTeaStashAsync(int userTeaStashId, int userId);
+        Task<BrewLog?> GetBrewLogByIdAsync(int brewLogId, int userId); // Get brew log by ID
+        Task CreateBrewLogAsync(BrewLog brewLog); // Create a new brew log
+        Task UpdateBrewLogAsync(BrewLog brewLog); // Update an existing brew log
+        Task DeleteBrewLogAsync(int id); // Delete a brew log by ID
+        Task<bool> SaveChangesAsync(); // For unit of work pattern
+    }
+}
