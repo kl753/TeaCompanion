39be1b4e03f472d8f7c7774b1ee58a2f955bc9ba
kl753 <cyberjade@gmail.com>
2025-06-18 19:59:54 -0500

    Add User Repositories.

---
+++ TeaAPI/Repositories/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿/*Repository interface for User
+ * Layer for abstracting data access logic.
+ * Interface for User repository.
+ */
+using TeaAPI.Models; 
+
+namespace TeaAPI.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync(); //Get all users
+        Task<User?> GetUserByIdAsync(int id); //Get user by ID
+        Task<User?> GetUserByUserameAsync(string username); //Get user by username
+        Task<User?> GetUserByEmailAsync(string email); //Get user by email
+        Task CreateUserAsync(User user); //Create a new user
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(User user); //Delete user by ID
+        Task<bool> SaveChangesAsync(); //Save changes to the database
+    }
+}
---
+++ TeaAPI/Repositories/UserRepository.cs
@@ -0,0 +1,60 @@
+﻿/*Layer abstracting data access logic.
+ * Implementation of IUserRepository repository.
+ */
+using Microsoft.EntityFrameworkCore;
+using TeaAPI.Models;
+using TeaAPI.Data;
+
+namespace TeaAPI.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+        
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+        
+        public async Task<User?> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+        
+        public async Task<User?> GetUserByUserameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+        }
+        
+        public async Task<User?> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+        
+        public async Task CreateUserAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+        }
+        
+        public async Task UpdateUserAsync(User user)
+        {
+            _context.Users.Update(user);
+            //SaveChangesAsync is called by service layer
+        }
+        
+        public async Task DeleteUserAsync(User user)
+        {
+            _context.Users.Remove(user);
+            //SaveChangesAsync is called by service layer
+        }
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0; // Returns true if any changes were saved
+        }
+    }
+}
