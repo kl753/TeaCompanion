de7e6d06c70b32a82b59231b79e814fa181777e4
kl753 <cyberjade@gmail.com>
2025-06-18 20:07:31 -0500

    Add User Service layer.

---
+++ TeaAPI/Services/IUserService.cs
@@ -0,0 +1,21 @@
+﻿/*Service layer interface
+ * Layer for abstracting business logic.
+ * Orchestrates calls to repository methods.
+ * Helps decouple controllers from data access logic.
+ * Interface for User service.
+ */
+using TeaAPI.DTOs;
+
+namespace TeaAPI.Services
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDto>> GetAllUsersAsync(); //Get all users
+        Task<UserDto?> GetUserByIdAsync(int id); //Get user by ID
+        Task<UserDto?> GetUserByUsernameAsync(string username); //Get user by username
+        Task<UserDto?> GetUserByEmailAsync(string email); //Get user by email
+        Task<UserDto> CreateUserAsync(CreateUserDto createUserDto); //Create a new user
+        Task<UserDto?> UpdateUserAsync(int id, UpdateUserDto updateUserDto); //Update user information
+        Task<bool> DeleteUserAsync(int id); //Delete user by ID
+    }
+}
---
+++ TeaAPI/Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿/*Service user implementation
+ * Layer for abstracting business logic.
+ * Orchestrates calls to repository methods.
+ * Helps decouple controllers from data access logic.
+ */
+using AutoMapper;
+using BCrypt.Net;
+using TeaAPI.DTOs;
+using TeaAPI.Models;
+using TeaAPI.Repositories;
+
+namespace TeaAPI.Services
+{
+    public class UserService : IUserService
+    {
+    }
+}
