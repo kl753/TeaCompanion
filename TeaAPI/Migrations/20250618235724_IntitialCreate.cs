ea6b75dcf2654463777ec10d5a492a975566da98
kl753 <cyberjade@gmail.com>
2025-06-18 19:19:25 -0500

    Create Migrations. Create User Entity.

---
+++ TeaAPI/Migrations/20250618235724_IntitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TeaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Type = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Subtype = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true),
+                    CountryOfOrigin = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    RecTemp = table.Column<int>(type: "integer", nullable: true),
+                    RecSteepTime = table.Column<int>(type: "integer", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Teas");
+        }
+    }
+}
---
+++ TeaAPI/Models/User.cs
@@ -0,0 +1,36 @@
+﻿/*Represents User table in the database.
+ * User.sql
+ */
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeaAPI.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //Auto-incrementing primary key
+        public int Id { get; set; }
+
+        [Required] //Not nullable
+        [MaxLength(50)] // Max length for username
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress] // Valid email format
+        [MaxLength(50)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        //Store password as a hash
+        [MaxLength(50)] 
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Default to current time
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow; // Default to current time
+
+        //Navigation properties for Tea Stash or Brew Logs
+        // public ICollection<UserTeaStashEntry>? TeaStash { get; set; }
+        // public ICollection<BrewSession>? BrewSession { get; set; }
+    }
+}
