670a7abb8ac74ab1262ac8d935128037763c7313
kl753 <cyberjade@gmail.com>
2025-06-19 11:55:36 -0500

    Add Mappings for User

--- TeaAPI/Mappers/MappingProfiles.cs
+++ TeaAPI/Mappers/MappingProfiles.cs
@@ -13,10 +13,22 @@ namespace TeaAPI.Mappers
         {
             // Create mappings between DTOs and entity models
             //Source -> Destination
+
+            //Tea Mappings
             CreateMap<Tea, TeaDto>()
                 .ReverseMap(); // Allows mapping in both directions
             CreateMap<CreateTeaDto, Tea>();
             CreateMap<UpdateTeaDto, Tea>();
+
+            //User Mappings
+            CreateMap<User, UserDto>(); //Don't reverse map, as UserDto doesn't have PasswordHash
+            CreateMap<CreateUserDto, User>()
+                .ForMember(dest => dest.PasswordHash, opt => opt.Ignore()); // Ignore PasswordHash during creation
+            CreateMap<CreateUserDto, User>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore()) // Ignore Id during creation
+                .ForMember(dest =>dest.PasswordHash, opt => opt.Ignore()) // Ignore PasswordHash during creation
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) // Ignore CreatedAt during creation
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null)); // Ignore null values
         }
     }
 }
