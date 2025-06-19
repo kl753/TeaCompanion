57f2662e9db05dedd7c735aaa51904ce6d2f7d94
kl753 <cyberjade@gmail.com>
2025-06-19 13:17:57 -0500

    Create a login DTO.

---
+++ TeaAPI/DTOs/LoginDto.cs
@@ -0,0 +1,16 @@
+﻿/*Transfer data class for login
+ * Used for POST requests to create a login
+ */
+using System.ComponentModel.DataAnnotations;
+
+namespace TeaAPI.DTOs
+{
+    public class LoginDto
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
